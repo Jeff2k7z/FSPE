1979fe418680e69fd3b870fcbd26c515536717f9
jbolton <jbolton@JBOLTONVM>
2016-07-24 13:37:49 -0400

    Add project files.

---
+++ FSPE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FSPE.Startup))]
+namespace FSPE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
